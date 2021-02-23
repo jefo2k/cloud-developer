199f034bf905bc56a41c044843bb05f7f40fb04a
Jeferson Santos <jefo2k@gmail.com>
2021-02-23 20:38:24 -0300

    add support to env variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "root",
-    "password": "root",
-    "database": "udacity",
-    "host": "localhost",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "jefoland",
-    "aws_media_bucket": "udagram-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
