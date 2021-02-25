b3ad8e945eb63241e32f1de096892aa792458140
Jeferson Santos <jefo2k@gmail.com>
2021-02-25 16:46:21 -0300

    add filteredimage endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,11 +13,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,8 +26,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url: imageUrl } = req.query;
 
-  //! END @TODO1
+    if (!imageUrl) {
+      return res.status(400).send({ message: 'image_url is required' });
+    }
+
+    try {
+      // generate and send the resulting filtered image
+      const filteredImage = await filterImageFromURL(imageUrl);
+      res.sendFile(filteredImage, async err => {
+        // remove image locally after image has sent
+        if (!err) {
+          await deleteLocalFiles([filteredImage]);
+        }
+      });
+
+    } catch (error) {
+      res.send({ 
+        status: 'error',
+        message: 'URL is not a valid publicly accessible image'
+      });
+    }
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
